7b73f22ef402797ec80238d3a578c151bc5da53f
guburchardt <guburchardt@hotmail.com>
2025-04-23 15:07:08 -0300

    Create usermodels and database configuration

---
+++ models/user.py
@@ -0,0 +1,8 @@
+from database import db
+from flask_login import UserMixin #Importa os metodos de Autenticacao
+
+#Colunas: id (int), username (text), password (text)
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True) #Declaracao de tipo
+    username = db.Column(db.String(80), nullable=False, unique=True) #nullable = False - Obriga usuario a nao deixar nulo
+    password = db.Column(db.String(80), nullable=False)
