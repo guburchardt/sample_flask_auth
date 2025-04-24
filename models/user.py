033d1a5b80fc02da601e0cf3b344d5beaed7024d
guburchardt <guburchardt@hotmail.com>
2025-04-24 16:21:49 -0300

    Create users roles

--- models/user.py
+++ models/user.py
@@ -6,3 +6,4 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True) #Declaracao de tipo
     username = db.Column(db.String(80), nullable=False, unique=True) #nullable = False - Obriga usuario a nao deixar nulo
     password = db.Column(db.String(80), nullable=False)
+    role = db.Column(db.String(80), nullable=False, default='user')
